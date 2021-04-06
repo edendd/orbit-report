5e07e236da279a1b1053f427661b1591abe5280f
edenhailu333@gmail.com <EDENhailu21>
2021-04-06 17:19:08 -0500

    last push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrbitListComponent
   ],
   imports: [
     BrowserModule,
